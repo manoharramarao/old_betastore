0d563be6748d4e28732f5123ecc727ce93bdba17
manohar <manohar@manohar-VirtualBox.dev.local>
2014-11-01 19:26:17 -0400

    categories getting pulled from DB

---
+++ web2py/applications/betastore/controllers/bisineer_categories.py
@@ -0,0 +1,20 @@
+import logging
+import gluon.contrib.simplejson as json
+
+logger = logging.getLogger("bisineer_categories")
+logger.setLevel(logging.DEBUG)
+
+def get_categories():
+	try:
+		result = {}
+		query = db.bis_category.id > 0
+		fields = ['name']
+		sel = [db.bis_category[field] for field in fields]
+		rows = db(query).select(*sel, projection=True)
+		print rows
+		#rows = db(db.bis_category.ALL).select(*sel, projection=True).as_dict()
+		result['categories'] = rows
+	except Exception, e:
+		logging.error(str(e))
+		result = {"result": "failure", "msg": "Something went wrong. Please try again"}
+	return result
--- web2py/applications/betastore/controllers/bisineer_products.py
+++ web2py/applications/betastore/controllers/bisineer_products.py
@@ -5,30 +5,26 @@ logger = logging.getLogger("bisineer_products")
 logger.setLevel(logging.DEBUG)
 
 def get_products():
-	"""
-	result: {
-		count: 1000,
-
-
-	}
-	"""
 	try:
 		result = {}
 		query = db.bis_product.id > 0
-		fields = ['name','unit_price', 'description_short']
-		none_fields = ['categories', 'specifications', 'weight', 'product_attributes', 'volume', 'bis_category', 'variant_products', 'description_long', 'tax_rate', 'in_stock', '']
+		fields = ['name','unit_price', 'description_short', 'rating', 'image_urls','id']
+		#none_fields = ['categories', 'specifications', 'weight', 'product_attributes', 'volume', 'bis_category', 'variant_products', 'description_long', 'tax_rate', 'in_stock', '']
 		sel = [db.bis_product[field] for field in fields]
-		rows = db().select(db.bis_product.ALL, cache=(cache.ram,60), cacheable=True, orderby=db.bis_product.name, limitby=(0,20))
+		rows = db(query).select(*sel, projection=True)
+		#rows = db().select(db.bis_product.ALL, cache=(cache.ram,60), cacheable=True, orderby=db.bis_product.id, limitby=(0,20))
+		#pages = count/20
 		# this for loop is because of gae with web2py. If we have to select specific fields, 
 		#then we need to use projection=True in the query. But if we use projection=True, it doesn't allow to use limitby
-		if bool(rows):
-			for row in rows:
-				for field in none_fields:
-					row[field] = None
+		#if bool(rows):
+		#	for row in rows:
+		#		for field in none_fields:
+		#			row[field] = None
 		#rows = db().select(*sel, projection=True, cache=(cache.ram,60), cacheable=True) # use this for caching
-		print rows
+		#print rows
 		if bool(rows):
 			result['products'] = rows
+			#result['pages'] = pages
 		else:
 			result = {"result": "failure", "msg": "no products available"}
 	except Exception, e:
